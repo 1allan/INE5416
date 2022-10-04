f6da195fde8212b7436079b7d2eccc7dd8bfeff6
Allan <allan.nwrk@gmail.com>
2022-10-03 21:15:01 -0300

    Fix columnAt and implement regionAt

--- T1/Sudoku.hs
+++ T1/Sudoku.hs
@@ -1,15 +1,49 @@
+module Sudoku where
+import Data.Set (fromList)
+import Data.Tuple (swap)
+
 data Cell = Cell {value::Int, right::Bool, bottom::Bool}
+    deriving(Show, Eq)
 type Line = [Cell]
 type Board = [Line]
 
+
+m :: [[Int]]
+m = [
+    [5, 0, 9, 6, 0, 0, 0, 0, 0],
+    [0, 2, 1, 0, 0, 0, 0, 0, 8],
+    [0, 0, 6, 5, 0, 0, 0, 0, 0],
+    [7, 0, 5, 0, 1, 0, 0, 2, 0],
+    [0, 6, 0, 0, 0, 0, 0, 4, 0],
+    [0, 1, 0, 0, 2, 0, 8, 0, 7],
+    [0, 0, 0, 0, 0, 5, 7, 0, 0],
+    [2, 0, 0, 0, 0, 0, 4, 8, 0],
+    [0, 0, 0, 0, 0, 0, 2, 0, 5]]
+
+dummy :: Int -> Cell
+dummy v = Cell v False False
+
+b :: Board
+b = map (map dummy) m
+
+itop :: Int -> (Int, Int)
+itop i = swap $ i `divMod` 8
+
+ptoi :: (Int, Int) -> Int
+ptoi (x, y) = y * 9 + x
+
 rowAt :: Board -> Int -> Line
 rowAt m i
-    | i >= length m = []
+    | i < 0 || i >= length m = []
     | otherwise = m !! i
 
 columnAt :: Board -> Int -> Line
-columnAt = go where
-    go [] _ = []
-    go (x:xs) i
-        | i >= length x = []
-        | otherwise = x !! i : go xs i
+columnAt [] _ = []
+columnAt (x:xs) i
+    | i < 0 || i >= length x = []
+    | otherwise = x !! i : columnAt xs i
+
+regionAt :: Board -> (Int, Int) -> Line
+regionAt b (x, y) =
+    let rows = drop (y `div` 3 * 3) in
+    concatMap (take 3) (take 3 (rows b))
--- T1/TODO.md
+++ T1/TODO.md
@@ -1,5 +1,5 @@
 # TODO
-- [ ] Ver com o professor se o tamanho da matrize é realmente *até* 9 (ou se é só para aceitar 4, 6 ou 9) e como funciona o dimensionamento das regiões
+- [x] Ver com o professor se o tamanho da matrize é realmente *até* 9 (ou se é só para aceitar 4, 6 ou 9) e como funciona o dimensionamento das regiões
 - [ ] Parsear arquivos txt
     - Vide a.txt
     - Linha 1 tamanho da matriz NxN
