899d0c4221d940cadb515da2a6aaf9593662edf0
Allan Soares Silva <allan.nwrk@gmail.com>
2022-10-18 21:15:18 -0300

    Remove os números da especificação do txt

--- T1/main.hs
+++ T1/main.hs
@@ -5,7 +5,7 @@ import Data.List (splitAt)
 
 parse :: String -> Board
 parse str = listToBoard cells where
-    cells = map (\(v:r:b) -> Cell (digitToInt v) r (head b)) (words str)
+    cells = map (\(r:b) -> Cell 0 r (head b)) (words str)
 
 main :: IO ()
 main = do
