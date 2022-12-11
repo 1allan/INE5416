0c3be4805619c0e3a3bd0ac9769288fd215e4906
eduardobetim <edubetimso@gmail.com>
2022-12-11 14:20:43 -0300

    prolog funcs

--- T3/sudoku.pl
+++ T3/sudoku.pl
@@ -91,7 +91,7 @@ slice(Board, From, To, Return):-
 # ?- slice([a,b,c,d,e], 1, 4, Return).
 # Return = [b, c].
 
-# retornta um Board de uma lista de lines
+# retorna um Board de uma lista de lines
 listToBoard(Lista).
     Board(slice(Lista,0, 8, Return1), slice(Lista,9, 17, Return2),
      slice(Lista,18, 27, Return3), slice(Lista,28, 36, Return4), slice(Lista,37,45, Return5), slice(Lista,46,54, Return6),
@@ -166,7 +166,8 @@ rowAt(Board, Int).
 #     (reverse (reccc nil (copy-list board))))
 
 columnAt(Board, Int).
-() :- columnAt().
+(fail) :- columnAt((fail) :- column(board, i),((i < 0);(:-(i>=(length(board))))).).
+(board[i]):- column(board, i).  # mesma logica do rowAt
 
 
 
@@ -190,17 +191,73 @@ columnAt(Board, Int).
 #         (map 'list (lambda (r) (subseq (subseq r x) 0 3)) rows)))
 
 regionAt(Board, coord).
+(x, floor(first coord) :- (x `div` 3 * 3)
+(y, floor(first coord) :- (y `div` 3 * 3)
 
  
 
-
-
 # (defun replace_ (l e i)
-#     ...
+# replace :: [a] -> a -> Int -> [a]
+# replace l e i
+#     | i >= 0 && i < length l = take i l ++ [e] ++ drop (i + 1) l
+#     | otherwise = l
+
+replace(l, e)
+drop (i + 1) :- ((i >= 0);(:-(i < length(l))))
 
 
 # (defun possibilities (b_ index)
 #     ...
+# possibilities :: [Cell] -> Int -> [Int]
+# possibilities b' index =
+#     let
+#         coord@(x, y) = itop index
+
+#         b = listToBoard b'
+#         currentCell = cellAt b coord
+#         upperCell = if y > 0 then cellAt b (x, y - 1) else Cell 0 '.' '.'
+#         leftCell = if x > 0 then cellAt b (x - 1, y) else Cell 0 '.' '.'
+
+#         assert v = all (\cond -> cond v) [rightOp leftCell, bottomOp upperCell]
+
+#         usedValues = map value (rowAt b y ++ columnAt b x ++ regionAt b coord)
+
+#         operations = [inverseOf (right leftCell), inverseOf (bottom upperCell), right currentCell, bottom currentCell]
+#         minValue = 1 + length (filter (=='+') operations)
+#         maxValue = 9 - length (filter (=='-') operations)
+
+#         possibilities = filter (`notElem` usedValues) [minValue .. maxValue]
+#     in
+#     filter assert possibilities
+possibilities(b)
+listToBoard(b2, b)
+cellAt(x,y - 1) :- (y > 0)
+leftCell(x - 1, y) :- (x > 0)
+(filter (=='+') operations, minValue)
+(filter (=='+') operations, maxValue)
 
 # (defun solve (board)
 #     ...
+# solve :: Board -> Board
+# solve b = let
+#         board = boardToList b
+#         possMatrix = map (const []) board
+#     in
+#     go board possMatrix 0 True where
+#         go b _ (-1) _ = listToBoard b
+#         go b _ 81 _ = listToBoard b
+#         go b b' i forward = let
+#                 poss = if forward then possibilities b i else b' !! i
+#             in
+#             case poss of
+#                 [] -> let
+#                         currCell = b !! i
+#                         emptyCell = Cell 0 (right currCell) (bottom currCell)
+#                     in
+#                     go (replace b emptyCell i) b' (i - 1) False
+#                 _  -> let
+#                         (x:xs) = poss
+#                         currCell = b !! i
+#                         newCell = Cell x (right currCell) (bottom currCell)
+#                     in
+#                     go (replace b newCell i) (replace b' xs i) (i + 1) True
