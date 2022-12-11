4694d2f951a3076fcb22e0e37cc2177b219b740c
eduardobetim <edubetimso@gmail.com>
2022-12-11 17:38:25 -0300

    adding comments

--- T3/sudoku.pl
+++ T3/sudoku.pl
@@ -20,6 +20,7 @@ partialOp(right, value) :- rightOp(Cell(value , right , bottom)).
 bottomOp(Cell).
 partialOp(bottom, value) :- bottomOp(Cell(value , right , bottom)).
 
+#converte o tabuleiro para formato de lista
 boardToList(Board).
 append(append(append(append(append(append(append(append(Line1, Line2, Result2), Line3, Result3), Line4, Result4), Line5, Result5), Line6, Result6), Line7, Result7), Line8, Result8), Line9, Result9) :- 
 boardToList(Board([Line1, Line2, Line3, Line4, Line5, Line6, Line7, Line8, Line9])).
@@ -30,6 +31,7 @@ slice(Board, From, To, Return):-
     append(LTo, _, L),
     append(LFrom, Return, LTo).
 
+# converte a lista para formato de tabuleiro
 listToBoard(Lista).
     Board(slice(Lista,0, 8, Return1), slice(Lista,9, 17, Return2),
      slice(Lista,18, 27, Return3), slice(Lista,28, 36, Return4), slice(Lista,37,45, Return5), slice(Lista,46,54, Return6),
@@ -38,17 +40,21 @@ listToBoard(Lista).
 itop(Int).
 (i mod 9)(floor(i)):- itop(i).
 
+
 cellAt(board, coord).
 (board, (x,y)) :- ((board, (x, y)), (x \= y) ).
 
+# retorna uma célula presente em dada linha
 rowAt(Board, Int).
 (fail) :- rowAt(board, i),((i < 0);(:-(i>=(length(board))))).
 (board[i]):- rowAt(board, i).  
 
+# retorna uma célula presente em dada coluna
 columnAt(Board, Int).
 (fail) :- columnAt((fail) :- column(board, i),((i < 0);(:-(i>=(length(board))))).).
 (board[i]):- column(board, i). 
 
+# retorna uma célula presente em dada região
 regionAt(Board, coord).
 (x, floor(first coord) :- (x `div` 3 * 3)
 (y, floor(first coord) :- (y `div` 3 * 3)
@@ -56,7 +62,7 @@ regionAt(Board, coord).
 replace(l, e)
 drop (i + 1) :- ((i >= 0);(:-(i < length(l))))
 
-
+# gera a lista de possibilidades
 possibilities(b)
 listToBoard(b2, b)
 cellAt(x,y - 1) :- (y > 0)
